be70fa23d490a3b34818c3b94a436aed90ab07cc
Rodrigo Llull <rodrigollullrodriguez@gmail.com>
2024-05-26 11:07:18 -0300

    Modelando la clase Turno

---
+++ src/main/java/com/backend/clinica_odontologica/entity/Turno.java
@@ -0,0 +1,53 @@
+package com.backend.clinica_odontologica.entity;
+
+import java.time.LocalDate;
+
+public class Turno {
+
+    private Long id;
+    private Paciente paciente;
+    private Odontologo odontologo;
+    private LocalDate fecha;
+
+    public Turno() {
+    }
+
+    public Turno(Long id, Paciente paciente, Odontologo odontologo, LocalDate fecha) {
+        this.id = id;
+        this.paciente = paciente;
+        this.odontologo = odontologo;
+        this.fecha = fecha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Odontologo getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+}
